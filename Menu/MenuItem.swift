424cd43686711ecef68c6d6eba791b33e9c6b5ba
Mark Ballereau <mark.b@live.fr>
2023-07-01 19:01:22 +0200

    Add MenuItem structure

---
+++ Menu/MenuItem.swift
@@ -0,0 +1,14 @@
+//
+//  MenuItem.swift
+//  Menu
+//
+//  Created by Mark Ballereau on 01/07/2023.
+//
+
+import Foundation
+
+struct MenuItem {
+    var name: String
+    var price: String
+    var imageName: String
+}
