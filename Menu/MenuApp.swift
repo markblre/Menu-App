dbe300e1e8fa732449df3d06eeb6d7c4f13dcfc3
Mark Ballereau <mark.b@live.fr>
2023-07-01 18:24:27 +0200

    Initial Commit

---
+++ Menu/MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  MenuApp.swift
+//  Menu
+//
+//  Created by Mark Ballereau on 01/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
